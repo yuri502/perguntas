6b179a51406d1deaaafe493b8a32a34db8d4964c
yuri <yuriarakaki502@gmail.com>
2024-11-19 14:19:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fisica',
+    'qual a primeira lei de ohm?',
+    'a corrente elétrica é diretamente proporcional à diferença de potência aplicada'
+)
+
+criaCartao(
+    'biologia',
+    'quem é o pai da genética?',
+    'Gregor mendel'
+)
+
+criaCartao(
+    'matemática',
+    'qual a raiz de 2?',
+    '1,41'
+)
+
+criaCartao(
+    'Química',
+    'qual a fórmula química do sal de cozinha?',
+    'NaCl (cloreto de sódio)'
+)
